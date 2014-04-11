08f71c80e3b3757044e5f58bd7bf8bc2fdb95af0
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2014-04-11 16:21:07 +0200

    m

--- init/80_create_instruments.py
+++ init/80_create_instruments.py
@@ -2,7 +2,7 @@ import logging
 sys.path.append('D:\\measuring\\measurement')
 
 
-#remote instrument connection 
+#remote instrument connection
 def _do_remote_connect():
     global TemperatureController_lt1, AttoPositioner_lt1
     if objsh.start_glibtcp_client('192.168.0.20',port=12002, nretry=3, timeout=5):
@@ -14,19 +14,19 @@ def _do_remote_connect():
 
         return True
 
-    logging.warning('Failed to start remote instruments')       
+    logging.warning('Failed to start remote instruments')
     return False
 
-def remote_connect_pids():
-    global pidyellow,pidmatisse,pidnewfocus
-    if objsh.start_glibtcp_client('192.168.0.80',port=12002, nretry=3, timeout=5):
-        remote_ins_server_lasers=objsh.helper.find_object('qtlab_lasermeister:instrument_server')
-        pidyellow = qt.instruments.create('pidyellow', 'Remote_Instrument',
-                     remote_name='pidyellow', inssrv=remote_ins_server_lasers)    
-        pidmatisse = qt.instruments.create('pidmatisse', 'Remote_Instrument',
-                     remote_name='pidmatisse', inssrv=remote_ins_server_lasers)
-        pidnewfocus = qt.instruments.create('pidnewfocus', 'Remote_Instrument',
-                     remote_name='pid_lt2_newfocus', inssrv=remote_ins_server_lasers)                       
+# def remote_connect_pids():
+#     global pidyellow,pidmatisse,pidnewfocus
+#     if objsh.start_glibtcp_client('192.168.0.80',port=12002, nretry=3, timeout=5):
+#         remote_ins_server_lasers=objsh.helper.find_object('qtlab_lasermeister:instrument_server')
+#         pidyellow = qt.instruments.create('pidyellow', 'Remote_Instrument',
+#                      remote_name='pidyellow', inssrv=remote_ins_server_lasers)
+#         pidmatisse = qt.instruments.create('pidmatisse', 'Remote_Instrument',
+#                      remote_name='pidmatisse', inssrv=remote_ins_server_lasers)
+#         pidnewfocus = qt.instruments.create('pidnewfocus', 'Remote_Instrument',
+#                      remote_name='pid_lt2_newfocus', inssrv=remote_ins_server_lasers)
 
 # adwin
 physical_adwin = qt.instruments.create('physical_adwin','ADwin_Pro_II'
@@ -34,30 +34,30 @@ physical_adwin = qt.instruments.create('physical_adwin','ADwin_Pro_II'
 adwin = qt.instruments.create('adwin', 'adwin_lt2', init=False, use_cfg=False)
 
 
-lt1_control=True
+lt1_control=False
 #labjack
 # NOTE not connected right now
 #labjack = qt.instruments.create('labjack', 'LabJack_U3')
 
 # laser & wavemeter
-# NewfocusLaser = qt.instruments.create('NewfocusLaser', 'NewfocusVelocity', 
+# NewfocusLaser = qt.instruments.create('NewfocusLaser', 'NewfocusVelocity',
 #        address = 'GPIB::10::INSTR' )
 
 #pids
-remote_connect_pids()        
-        
-tuner=qt.instruments.create('tuner','lt2_ssro_optimizer')                
- 
-#remote pid yellow:      
+#remote_connect_pids()
+
+#tuner=qt.instruments.create('tuner','lt2_ssro_optimizer')
+
+#remote pid yellow:
 
 #ivvi rack
-ivvi = qt.instruments.create('ivvi', 'IVVI', address = 'ASRL1::INSTR', numdacs= 4)
+#ivvi = qt.instruments.create('ivvi', 'IVVI', address = 'ASRL1::INSTR', numdacs= 4)
 
 if lt1_control:
     physical_adwin_lt1 = qt.instruments.create('physical_adwin_lt1','ADwin_Gold_II',
                      address=353)
-    adwin_lt1 = qt.instruments.create('adwin_lt1', 'adwin_lt1', init=False, 
-            use_cfg=False) 
+    adwin_lt1 = qt.instruments.create('adwin_lt1', 'adwin_lt1', init=False,
+            use_cfg=False)
     #remote_ins_connect=_do_remote_connect
     # _monitor_lt1 = remote_ins_connect()
 
@@ -69,7 +69,7 @@ gmailer = qt.instruments.create('gmailer','send_email',username='cryolt2',passwo
 monitor_cryo = qt.instruments.create('monitor_cryo','monitor_cryo', monitor_lt1=False)
 monitor_cryo.start()
 
-#adwin_lt2_monit0r = qt.instruments.create('adwin_lt2_monit0r', 
+#adwin_lt2_monit0r = qt.instruments.create('adwin_lt2_monit0r',
 #    'adwin_monit0r', physical_adwin='physical_adwin')
 #adwin_lt2_monit0r.add_fpar(41, 'Matisse Frequency')
 #adwin_lt2_monit0r.add_fpar(42, 'Yellow Frequency')
